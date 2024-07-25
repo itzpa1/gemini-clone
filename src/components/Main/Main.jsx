3d3d495b6c3931b39b6e0c1b9a7c9b9faf7b3874
Pawan Kumar <65233567+itzpa1@users.noreply.github.com>
2024-07-25 14:08:40 +0530

    Added Dark Theme Changed user icon

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,34 +9,34 @@ const Main = () => {
 
     return (
         <div className='main flex-1 min-h-[100vh] relative pb-[15vh] '>
-            <div className="nav flex items-center justify-between text-[22px] p-[20px] text-[#585858]  ">
-                <p>Gemini</p>
-                <img src={assets.user_icon} alt="" />
+            <div className="nav flex items-center justify-between text-[22px] p-[20px]  ">
+                <p className='cursor-pointer'>Gemini</p>
+                <img src={assets.user_icon} title='User' alt="" />
             </div>
             <div className="main-container max-w-[900px] m-auto ">
 
                 {!showResult
                     ? <>
 
-                        <div className="greet mx-0 my-[50px] text-[56px] text-[#c4c7c5] font-medium p-[20px] ">
-                            <p><span>Hello, Dev.</span></p>
+                        <div className="greet mx-0 my-[50px] text-[56px] font-medium p-[20px] ">
+                            <p><span>Hello, Pawan.</span></p>
                             <p>How can I heelp you today?</p>
                         </div>
                         <div className="cards">
                             <div className="card">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, dolore.</p>
+                                <p>Suggest beautiful places to see an upcoming road trip</p>
                                 <img src={assets.compass_icon} alt="" />
                             </div>
                             <div className="card">
-                                <p>dolor sit amet consectetur adipisicing elit. Cumque, dolore.</p>
+                                <p>Briefly summarize this concept urban planning</p>
                                 <img src={assets.bulb_icon} alt="" />
                             </div>
                             <div className="card">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, dolore.</p>
+                                <p>Brainstrom team bonding activities for our work retreat</p>
                                 <img src={assets.message_icon} alt="" />
                             </div>
                             <div className="card">
-                                <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, dolore.</p>
+                                <p>Imporve the readability of the following code</p>
                                 <img src={assets.code_icon} alt="" />
                             </div>
                         </div>
@@ -64,15 +64,15 @@ const Main = () => {
 
 
                 <div className="main-bottom absolute bottom-0 w-[100%] max-w-[900px] py-0 px-5 m-auto ">
-                    <div className="search-box flex items-center justify-between gap-5 bg-[#f0f4f9] px-5 py-[10px] rounded-[50px] ">
+                    <div className="search-box flex items-center justify-between gap-5 px-5 py-[10px] rounded-[50px] ">
                         <input onChange={(e) => setInput(e.target.value)} value={input} className='flex-1  bg-transparent rounded-none outline-none p-2 text-[18px] ' type="text" placeholder='Enter a prompt here' />
                         <div className='flex items-center gap-[15px]  '>
-                            <img src={assets.gallery_icon} alt="" />
-                            <img src={assets.mic_icon} alt="" />
-                            {input?<img onClick={() => onSent()} src={assets.send_icon} alt="" />:null}
+                            <img src={assets.gallery_icon} title='Choose files' alt="" />
+                            <img src={assets.mic_icon} title='Mic' alt="" />
+                            {input?<img onClick={() => onSent()} src={assets.send_icon} title='Send' alt="" />:null}
                         </div>
                     </div>
-                    <p className="bottom-info text-[13px] mx-auto my-[15px] font-light ">
+                    <p className="bottom-info text-[13px] mx-auto my-[15px] font-light text-center ">
                         Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
                     </p>
                 </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,39 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import useLocalStorage from 'use-local-storage'
 import {assets} from '../../assets/assets'
 import { Context } from '../../context/Context'
+import { MdOutlineDarkMode,MdOutlineLightMode } from "react-icons/md";
+
 
 const sidebar = () => {
 
-    const [extended, setExtended] = useState(false)
+    const [extended, setExtended] = useState(false);
+    const [isDark, setIsDark] = useState('dark');
     const {onSent,prevPrompts,setRecentPrompt,newChat} = useContext(Context)
     
     const loadPrompt = async(prompt) => {
         setRecentPrompt(prompt)
         await onSent(prompt)
     }
+
+    const changeTheme = ()=> {
+        if(isDark === "dark"){
+            setIsDark('light')
+        } 
+        else {
+            setIsDark('dark')
+        } 
+    }
+
+    useEffect(() =>{
+        document.querySelector("#root").className=isDark;
+    },[isDark])
     
   return (
-    <div className='sidebar min-h-[100vh] inline-flex flex-col justify-between bg-[#f0f4f9] py-[25px] px-[15px]'>
+    <div className='sidebar min-h-[100vh] inline-flex flex-col justify-between py-[25px] px-[15px]'>
         <div className="top">
-            <img onClick={()=>setExtended(prev=>!prev)} className='menu block ml-[10px] cursor-pointer' src={assets.menu_icon} alt="" />
-            <div onClick={()=>newChat()} className="new-chat mt-[50px] inline-flex items-center gap-[10px] py-[10px] px-[15px] bg-[#e6eaf1] rounded-[50px] text-md text-zinc-500 cursor-pointer">
+            <img title='Exapand Menu' onClick={()=>setExtended(prev=>!prev)} className='menu block ml-[10px] cursor-pointer' src={assets.menu_icon} alt="" />
+            <div title='New Chat' onClick={()=>newChat()} className="new-chat mt-[50px] inline-flex items-center gap-[10px] py-[10px] px-[15px] rounded-[50px] text-md text-zinc-500 cursor-pointer">
                 <img src={assets.plus_icon} alt="" />
                 {extended?<p>New Chat</p>:null}
             </div>
@@ -38,15 +55,21 @@ const sidebar = () => {
             
         </div>
         <div className="bottom flex flex-col">
-            <div className="bottom-item recent-entry">
+            <div title='Theme' onClick={changeTheme} className="bottom-item toggle-theme recent-entry">
+                    {isDark === 'dark'
+                    ? <MdOutlineLightMode size="22px" />
+                    :<MdOutlineDarkMode size="22px" />}
+                {extended?<p>Theme</p>:null}
+            </div>
+            <div title='History' className="bottom-item recent-entry">
                 <img src={assets.history_icon} alt="" />
                 {extended?<p>History</p>:null}
             </div>
-            <div className="bottom-item recent-entry">
+            <div title='Activity' className="bottom-item recent-entry">
                 <img src={assets.question_icon} alt="" />
                 {extended?<p>Activity</p>:null}
             </div>
-            <div className="bottom-item recent-entry">
+            <div title='Settings' className="bottom-item recent-entry">
                 <img src={assets.setting_icon} alt="" />
                 {extended?<p>Settings</p>:null}
             </div>
